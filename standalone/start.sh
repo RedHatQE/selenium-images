c02ee8effa7327398d7c44f936c01d0516221cbe
Dmitry Misharov <dmisharo@redhat.com>
2020-04-10 22:00:13 +0200

    Removed xstartup and vnc config file; bumped firefox esr version

---
+++ standalone/start.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+trap cleanup 1 2 3 6
+
+cleanup() {
+# we would like to shutdown everything gracefully in the right order
+    pkill java
+    pkill fluxbox
+    pkill Xvnc
+}
+
+vncserver $DISPLAY -noxstartup \
+                   -securitytypes none \
+                   -geometry 1600x900 \
+                   -depth 16 \
+                   -alwaysshared &
+
+sleep 3
+xsetroot -solid grey
+startfluxbox -display $DISPLAY > /dev/null 2>&1 &
+java -jar $SELENIUM_PATH -port $SELENIUM_PORT 2>&1
