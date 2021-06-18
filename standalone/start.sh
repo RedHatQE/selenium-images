547ca543b352e4e6b257cfa12086f1c284653096
Dmitry Misharov <dmisharo@redhat.com>
2021-06-18 17:14:07 +0200

    Bumped version of the base fedora image

--- standalone/start.sh
+++ standalone/start.sh
@@ -1,21 +1,8 @@
 #!/bin/sh
 
-trap cleanup 1 2 3 6
+if [[ $VNC_GEOMETRY ]]; then
+    sed -i "s/geometry=.*/geometry=$VNC_GEOMETRY/g" "$HOME"/.vnc/config
+fi
 
-cleanup() {
-# we would like to shutdown everything gracefully in the right order
-    pkill java
-    pkill fluxbox
-    pkill Xvnc
-}
-
-vncserver $DISPLAY -noxstartup \
-                   -securitytypes none \
-                   -geometry "${VNC_GEOMETRY:-1600x900}" \
-                   -depth 16 \
-                   -alwaysshared &
-
-sleep 3
-xsetroot -solid grey
-startfluxbox -display $DISPLAY > /dev/null 2>&1 &
+/usr/libexec/vncserver $DISPLAY > /dev/null 2>&1 &
 java -jar $SELENIUM_PATH -port $SELENIUM_PORT 2>&1
