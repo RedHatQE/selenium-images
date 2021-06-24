2c027a2abbb69a1a48449fc19990363ff177dcaf
Dmitry Misharov <dmisharo@redhat.com>
2021-06-24 15:36:30 +0200

    Manually start Xvnc and fluxbox to fix starting C session in OCP

--- standalone/start.sh
+++ standalone/start.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
-if [[ $VNC_GEOMETRY ]]; then
-    sed -i "s/geometry=.*/geometry=$VNC_GEOMETRY/g" "$HOME"/.vnc/config
-fi
+/usr/bin/Xvnc $DISPLAY \
+              -alwaysshared \
+              -depth 16 \
+              -geometry $VNC_GEOMETRY \
+              -securitytypes none \
+              -auth $HOME/.Xauthority \
+              -fp catalogue:/etc/X11/fontpath.d \
+              -pn \
+              -rfbport 59$(echo $DISPLAY| tr -d ":") \
+              -rfbwait 30000 > /dev/null 2>&1 &
 
-/usr/libexec/vncserver $DISPLAY > /dev/null 2>&1 &
+sleep 3
+
+startfluxbox > /dev/null 2>&1 &
+
 java -jar $SELENIUM_PATH -port $SELENIUM_PORT 2>&1
