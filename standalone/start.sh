f00dee43a475d87aac330f732b10221488a8c80e
Dmitry Misharov <dmisharo@redhat.com>
2021-08-11 15:45:17 +0200

    A hack preventing fluxbox coredump

--- standalone/start.sh
+++ standalone/start.sh
@@ -1,26 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
-trap cleanup 1 2 3 6
-cleanup() {
-    # we would like to shutdown everything gracefully in the right order
-    pkill --signal SIGTERM java && \
-    pkill --signal SIGTERM fluxbox && \
-    pkill --signal SIGTERM Xvnc
-}
-
-/usr/bin/Xvnc $DISPLAY \
-              -alwaysshared \
-              -depth 16 \
-              -geometry $VNC_GEOMETRY \
-              -securitytypes none \
-              -auth $HOME/.Xauthority \
-              -fp catalogue:/etc/X11/fontpath.d \
-              -pn \
-              -rfbport 59$(echo $DISPLAY| tr -d ":") \
-              -rfbwait 30000 > /dev/null 2>&1 &
+Xvnc ${DISPLAY} \
+     -alwaysshared \
+     -depth 16 \
+     -geometry ${VNC_GEOMETRY} \
+     -securitytypes none \
+     -auth ${HOME}/.Xauthority \
+     -fp catalogue:/etc/X11/fontpath.d \
+     -pn \
+     -rfbport 59$(echo ${DISPLAY} | tr -d ":") \
+     -rfbwait 30000 > /dev/null 2>&1 &
 
 sleep 3
 
 startfluxbox > /dev/null 2>&1 &
 
-java -jar $SELENIUM_PATH -port $SELENIUM_PORT 2>&1
+java -jar ${SELENIUM_PATH} -port ${SELENIUM_PORT} 2>&1 &
+
+# a hack preventing fluxbox core dump
+cat
